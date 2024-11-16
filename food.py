402605d54276c022e4397d4b9d481b282523b638
Andrew-Mysaka <andrewmysaka@gmail.com>
2024-11-16 18:27:45 +0200

    Initial commit

---
+++ food.py
@@ -0,0 +1,16 @@
+from random import randint
+from turtle import Turtle
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=1, stretch_wid=1)
+        self.color("blue")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        self.goto(randint(-280,280), randint(-280,280))
